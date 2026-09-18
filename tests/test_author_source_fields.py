bcaecb02a3d2406610867d7afd5c74f8bb2ea97d
Ghislain HAINE <94449556+g-haine@users.noreply.github.com>
2026-09-18 11:46:48 +0200

    cleanup: keep author tests canonical and domain-neutral

--- tests/test_author_source_fields.py
+++ tests/test_author_source_fields.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
 
-from dataclasses import replace
 import unittest
 
-from bibreview.compat import legacy_record_to_publication, publication_to_legacy
 from bibreview.model import Author
 from bibreview.pipeline.collect import build_publication
 
@@ -23,7 +21,7 @@ class AuthorSourceFieldTests(unittest.TestCase):
 
     def test_collection_preserves_crossref_author_source_fields(self) -> None:
         message = {
-            "title": ["Port-Hamiltonian systems"],
+            "title": ["Multiphysics systems"],
             "type": "journal-article",
             "author": [
                 {
@@ -37,7 +35,7 @@ class AuthorSourceFieldTests(unittest.TestCase):
             "created": {"date-parts": [[2026, 9, 17]]},
         }
 
-        publication = build_publication("10.1234/example", message, "port-hamiltonian-systems")
+        publication = build_publication("10.1234/example", message, "multiphysics-systems")
         author = publication.authors[0]
 
         self.assertEqual(author.given, "Ada")
@@ -54,7 +52,7 @@ class AuthorSourceFieldTests(unittest.TestCase):
 
     def test_collection_preserves_literal_author_name(self) -> None:
         message = {
-            "title": ["Port-Hamiltonian systems"],
+            "title": ["Multiphysics systems"],
             "type": "journal-article",
             "author": [
                 {
@@ -65,7 +63,7 @@ class AuthorSourceFieldTests(unittest.TestCase):
             "created": {"date-parts": [[2026, 9, 17]]},
         }
 
-        publication = build_publication("10.1234/example", message, "port-hamiltonian-systems")
+        publication = build_publication("10.1234/example", message, "multiphysics-systems")
         author = publication.authors[0]
 
         self.assertEqual(author.literal, "Example Research Consortium")
@@ -73,46 +71,6 @@ class AuthorSourceFieldTests(unittest.TestCase):
         self.assertIsNone(author.family)
         self.assertEqual(dict(author.source_fields), {"sequence": "additional"})
 
-    def test_legacy_author_extras_survive_canonical_name_change(self) -> None:
-        record = {
-            "doi": "10.1234/example",
-            "type": "journal-article",
-            "title": "Example",
-            "authors": [
-                {
-                    "given": "Ada",
-                    "family": "Lovelace",
-                    "ORCID": "example-orcid",
-                    "affiliation": [{"name": "Example Institute"}],
-                }
-            ],
-            "abstract": "",
-            "journal": "",
-            "year": "2026",
-            "volume": "",
-            "issue": "",
-            "pages": "",
-            "publisher": "",
-            "event": "",
-            "keywords": "",
-            "dateY": "2026",
-            "dateM": "9",
-            "dateD": "17",
-            "permalink": "example",
-            "references": [],
-        }
-        item = legacy_record_to_publication(record)
-        changed_author = replace(item.publication.authors[0], family="Byron")
-        changed = replace(item, publication=replace(item.publication, authors=(changed_author,)))
-
-        rendered = publication_to_legacy(changed)
-
-        self.assertEqual(rendered["authors"][0]["family"], "Byron")
-        self.assertEqual(rendered["authors"][0]["ORCID"], "example-orcid")
-        self.assertEqual(
-            rendered["authors"][0]["affiliation"],
-            [{"name": "Example Institute"}],
-        )
 
 
 if __name__ == "__main__":
